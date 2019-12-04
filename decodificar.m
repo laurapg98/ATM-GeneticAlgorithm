764a5b5c198c6688efa7c16b8d3822689d25d1a9
laurapg98 <laura.parga.lp@gmail.com>
2019-12-04 09:59:05 +0100

    akjfh

--- decodificar.m
+++ decodificar.m
@@ -1,4 +1,5 @@
 function [vel,angle] = decodificar(bits) % da el cambio de velocidad y de ángulo dado un vector de bits (long=5)
+    
     % 3 primeros bits --> velocidad
     if(bits(2)==0 && bits(3)==0)
         vel=0;
@@ -17,14 +18,13 @@ function [vel,angle] = decodificar(bits) % da el cambio de velocidad y de ángulo
     end
     
     % 2 últimos bits --> cambio angulo
-    if(bits(4)==0 && bits(5)==0)
-        angle=0;
-    end
     if(bits(4)==1 && bits(5)==0)
         angle=-30; % izquierda
-    end
-    if(bits(4)==0 && bits(5)==1)
+    elseif(bits(4)==0 && bits(5)==1)
         angle=+30; % derecha
+    else
+        angle=0;
     end
+    
 end
 
