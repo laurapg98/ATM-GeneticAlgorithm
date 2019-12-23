d6ad21fc631e4da86548250ceb307746f272d32e
laurapg98 <laura.parga.lp@gmail.com>
2019-12-23 16:28:47 +0100

    alkjde

--- Main.m
+++ Main.m
@@ -1,26 +1,39 @@
 %% SECTION 1: clean MATLAB
+
 clc
 clear all
 close all
 
+
 %% SECTION 2: read airspace from files (sector + flight plans)
+
 % fSector="sector"; % data file of sector     ---> NO EXISTE
 % LeerSector(fSector); %      ---> TAMPOCO EXISTE
+
 fFP="FlightPlans.txt"; % data file of flight plans
 [ListFPi,numFP]=ReadFP(fFP); 
+
+
 %% SECTION 3: create initial population
+
 numInd=100; % # individuals(=chromosomas=solutions)
 numBits=6; % # bits per FP (about our codification)
 
 population = RandomPopulation(numFP,numInd,numBits);
 
+
 %% SECTION 4: GA loop
+
 SecDistance=8; % security distance in NM
 SimTime=5; % time increment in seconds
 PercetageElitism=0.1;
+numElitism=PercetageElitism*numInd; % # individuals copied in the new population
+numNoElitism=numInd-numElitism; % # individuals that have to enter in the roulette
+
 %while(determinar cuándo parar):    
     FitnessVector=zeros(numInd,1);
     
+    % FITNESS
     for Chrom=1:1:numInd % computes the fitness of each individual of the population
         [ListFPm,AllVel,AllAngles,AllDist] = ModifyListFP(ListFPi,numInd,population(Chrom,:)); 
         numAffected = GetAffected(ListFPi, ListFPm,numFP);
@@ -28,7 +41,10 @@ PercetageElitism=0.1;
         FitnessVector(Chrom) = fitness(numAffected,numConflicts, AllVel,AllAng,AllDist,numInd,numFP);
     end
     
-    FitnessVector=sort(FitnessVector);
-    numElitism=PercetageElitism*length(FitnessVector);
+    % NEW POPULATION
+    FitnessVector=sort(FitnessVector); 
     Elitism=FitnessVector(1:numElitism,1);
+    NoElitism=FitnessVector(numElitism+1:numInd,1);
+    
+    % MUTATION
     
--- fitness.m
+++ fitness.m
@@ -2,7 +2,8 @@
 
 function fit = fitness(Affected,Conflicts, velocity_changes,heading_changes,distance_changes,numInd,numFP)
     
-    % A + B + C + D = 1 --> Gives weight to each one of the terms
+    % A + B + C + D = 1 --> Give weight to each one of the terms in the
+                            % fitness function:
     A=0.25; % weight of the number of affected aircrafts
     B=0.25; % weight of the changes on velocity
     C=0.25; % weight of the changes on heading
@@ -10,9 +11,9 @@ function fit = fitness(Affected,Conflicts, velocity_changes,heading_changes,dist
 
     %Computing the sum of velocity, heading and distance changes
     for i=1:1:numFP
-        sumVel= sumVel+velocity_changes(i);
-        sumAng= sumAng+heading_changes(i);
-        sumDist= sumDist+distance_changes(i);
+        sumVel= sumVel+abs(velocity_changes(i));
+        sumAng= sumAng+abs(heading_changes(i));
+        sumDist= sumDist+abs(distance_changes(i));
     end
 
     %Fitness function
